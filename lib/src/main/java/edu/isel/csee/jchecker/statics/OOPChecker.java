904b81867d69ba48b5256e197fd61b7aea65c163
newForinux <you_maybe@naver.com>
2021-01-27 15:49:22 +0900

    [REFACTOR] add test methods about overriding, overloading.

--- lib/src/main/java/edu/isel/csee/jchecker/statics/OOPChecker.java
+++ lib/src/main/java/edu/isel/csee/jchecker/statics/OOPChecker.java
@@ -1,23 +1,57 @@
 package edu.isel.csee.jchecker.statics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jdt.core.dom.*;
+import com.google.gson.JsonObject;
+import edu.isel.csee.jchecker.score.EvaluationSchemeMapper;
 
 
 
 public class OOPChecker extends ASTChecker {
+	private EvaluationSchemeMapper policy;
 	private List<IMethodBinding> methods = new ArrayList<>();
 	private List<String> invocations = new ArrayList<>();
 	private List<String> packages = new ArrayList<>();
 	private List<String> classes = new ArrayList<>();
+	private Map<String, String> superclasses = new HashMap<>();
+	private Map<String, String> interfaces = new HashMap<>();
+	
+	private ArrayList<String> runtimeViolations = null;
+	private ArrayList<String> classesViolations = null;
+	private ArrayList<String> methodsViolations = null;
+	private ArrayList<String> customExcViolations = null;
+	private ArrayList<String> customStrViolations = null;
+	private ArrayList<String> spcViolations = null;
+	private ArrayList<String> itfViolations = null;
+	
+	private boolean pkgViolation = false;
+	private boolean ovrViolation = false;
+	private boolean ovlViolation = false;
+	private boolean jvdViolation = false;
+	private boolean ecpViolation = false;
 	
 	
-	private boolean violation_cap = false;
+	private int runtimeViolationCount = 0;
+	private int classesViolationCount = 0;
+	private int methodsViolationCount = 0;
+	private int spcViolationCount = 0;
+	private int itfViolationCount = 0;
+	
+	
+	private JsonObject scoresheet;
 	
 	
 	
+	
+	public OOPChecker(EvaluationSchemeMapper policy)
+	{
+		this.policy = policy;
+		
+	}
 	
 	
 	public void run(List<String> source, String unitName)
@@ -35,10 +69,31 @@ public class OOPChecker extends ASTChecker {
 	
 	
 	
+	private void testSuperclass(CompilationUnit unit)
+	{
+		try {
+			unit.accept(new ASTVisitor() {
+				public boolean visit(TypeDeclaration node)
+				{
+					if (!policy.getSuperclass_pair().containsKey(node.getName().toString()))
+						return super.visit(node);
+					
+					if (policy.getSuperclass_pair().get(node.getName().toString()).equals(node.getSuperclassType().toString())) {
+						spcViolations.add(policy.getSuperclass_pair().get(node.getName().toString()));
+						spcViolationCount++;
+					}
+					
+					
+					return super.visit(node);
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 	
-	/* TODO: input / output format 정의 필요
 	
 	private void testInterface(CompilationUnit unit)
 	{
@@ -47,7 +102,22 @@ public class OOPChecker extends ASTChecker {
 			unit.accept(new ASTVisitor() {
 				public boolean visit(TypeDeclaration node)
 				{
+					boolean flag = false;
+					if (!policy.getInterface_pair().containsKey(node.getName().toString()))
+						return super.visit(node);
 					
+					for (Object each : node.superInterfaceTypes()) {
+						if (policy.getInterface_pair().get(node.getName().toString()).equals(each.toString())) {
+							flag = true;
+						}
+					}
+					
+					if (!flag) {
+						itfViolations.add(policy.getInterface_pair().get(node.getName().toString()));
+						itfViolationCount++;
+					}
+					
+					return super.visit(node);
 				}
 			});
 		} catch (Exception e) {
@@ -55,7 +125,6 @@ public class OOPChecker extends ASTChecker {
 		}
 	}
 	
-	*/
 	
 	
 	public void testOverriding(CompilationUnit unit)
@@ -65,6 +134,17 @@ public class OOPChecker extends ASTChecker {
 			unit.accept(new ASTVisitor() {
 				public boolean visit(MethodDeclaration node)
 				{
+					if (!policy.getOverriding().isEmpty()) {
+						if (policy.getOverriding().contains(node.getName().toString())) {
+							for (IMethodBinding each : methods)
+							{
+								if (node.resolveBinding().overrides(each)) {
+									policy.getOverriding().remove(node.getName().toString());
+									return false;
+								}
+							}
+						}
+					}
 					
 					return super.visit(node);
 				}
@@ -92,11 +172,9 @@ public class OOPChecker extends ASTChecker {
 					
 					for (FieldDeclaration eachField : node.getFields())
 					{
-						if ((eachField.getModifiers() & Modifier.PUBLIC) > 0) {
-							
-							//TODO: 어떻게 violations를 넘겨줄 것인가?
-							violation_cap = true;
-						}
+						if ((eachField.getModifiers() & Modifier.PUBLIC) > 0)
+							ecpViolation = true;
+						
 					}
 					
 					
@@ -107,9 +185,9 @@ public class OOPChecker extends ASTChecker {
 					}
 					
 					
-					if (public_count == 0) {
-						 violation_cap = true;
-					}
+					if (public_count == 0)
+						ecpViolation = true;
+					
 					
 					
 					return super.visit(node);
@@ -167,6 +245,7 @@ public class OOPChecker extends ASTChecker {
 				public boolean visit(TypeDeclaration node)
 				{
 					classes.add(node.getName().toString());
+					
 					return super.visit(node);
 				}
 			});
@@ -174,4 +253,5 @@ public class OOPChecker extends ASTChecker {
 			e.printStackTrace();
 		}
 	}
+
 }
