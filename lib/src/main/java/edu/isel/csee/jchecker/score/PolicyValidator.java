ba0541355aeb0d5bf521c7190a768ceb48d46129
Seo, Jueun <22000350@handong.ac.kr>
2022-08-10 17:16:21 +0900

    #4 - add validator for deduct point setting

---
+++ lib/src/main/java/edu/isel/csee/jchecker/score/PolicyValidator.java
@@ -0,0 +1,21 @@
+package edu.isel.csee.jchecker.score;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class PolicyValidator {
+    public void validator(JsonObject policy){
+        // validation for classes deduct point
+        JsonObject obj = new Gson().fromJson(policy.get("classes"), JsonObject.class);
+        if (!obj.has("deductPoint")) {
+            System.out.println("deductPoint (X)");
+            obj.addProperty("deductPoint", 0.0);
+            policy.add("classes", obj);
+        }
+        if (!obj.has("maxDeduct")) {
+            System.out.println("maxDeduct (X)");
+            obj.addProperty("maxDeduct", 0.0);
+            policy.add("classes", obj);
+        }
+    }
+}
