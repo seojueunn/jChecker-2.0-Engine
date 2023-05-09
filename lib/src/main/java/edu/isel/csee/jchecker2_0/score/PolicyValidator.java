2eff20a10c4da19ed29366de1edaeae703dd6140
Seo, Jueun <22000350@handong.ac.kr>
2023-05-09 23:52:58 +0900

    update the policyValidator

--- lib/src/main/java/edu/isel/csee/jchecker2_0/score/PolicyValidator.java
+++ lib/src/main/java/edu/isel/csee/jchecker2_0/score/PolicyValidator.java
@@ -1,4 +1,4 @@
-package edu.isel.csee.jchecker2_0.score;
+package edu.isel.csee.jchecker.score;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -42,32 +42,34 @@ public class PolicyValidator {
             policy.add("classes", obj);
         }
 
-        // validation for checksum data
+        // validation for checksum data (oracle state is true)
         obj = new Gson().fromJson(policy.get("oracle"), JsonObject.class);
-        inputs = new Gson().fromJson(obj.get("input"), new TypeToken<ArrayList<String>>() {
-        }.getType());
-        checksums = new Gson().fromJson(obj.get("checksum"), new TypeToken<ArrayList<String>>() {
-        }.getType());
+        if (!obj.get("state").toString().equals("false")) {
+            inputs = new Gson().fromJson(obj.get("input"), new TypeToken<ArrayList<String>>() {
+            }.getType());
+            checksums = new Gson().fromJson(obj.get("checksum"), new TypeToken<ArrayList<String>>() {
+            }.getType());
 
-        setHasChecksum(checksums);
-        // if we use checksum, the number of checksum data is equal to input data size
-        if (getHasChecksum()) {
-            for (int i = 0; i < checksums.size(); i ++) {
-                // replace null data to empty string
-                if (checksums.get(i) == null) {
-                    checksums.set(i, "");
+            setHasChecksum(checksums);
+            // if we use checksum, the number of checksum data is equal to input data size
+            if (getHasChecksum()) {
+                for (int i = 0; i < checksums.size(); i ++) {
+                    // replace null data to empty string
+                    if (checksums.get(i) == null) {
+                        checksums.set(i, "");
+                    }
                 }
-            }
 
-            // #8 - input size > checksum size -> add some empty string in checksum data
-            if (inputs.size() > checksums.size()) {
-                while (inputs.size() != checksums.size()) {
-                    checksums.add("");
+                // #8 - input size > checksum size -> add some empty string in checksum data
+                if (inputs.size() > checksums.size()) {
+                    while (inputs.size() != checksums.size()) {
+                        checksums.add("");
+                    }
                 }
-            }
 
-            policy.getAsJsonObject("oracle").remove("checksum"); // remove original checksum data
-            policy.getAsJsonObject("oracle").add("checksum", (new Gson()).toJsonTree(checksums)); // add new
+                policy.getAsJsonObject("oracle").remove("checksum"); // remove original checksum data
+                policy.getAsJsonObject("oracle").add("checksum", (new Gson()).toJsonTree(checksums)); // add new
+            }
         }
     }
 }
